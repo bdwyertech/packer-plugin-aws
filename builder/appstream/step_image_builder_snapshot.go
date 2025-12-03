246227995fb47c6956cf13415578d6c0b69904c3
Brian Dwyer <bdwyer@IEEE.org>
2025-12-03 15:26:26 -0500

    Update

--- builder/appstream/step_image_builder_snapshot.go
+++ builder/appstream/step_image_builder_snapshot.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/aws/aws-sdk-go-v2/service/appstream"
 	"github.com/aws/aws-sdk-go-v2/service/appstream/types"
+
 	"github.com/hashicorp/packer-plugin-sdk/multistep"
 	packersdk "github.com/hashicorp/packer-plugin-sdk/packer"
 )
@@ -36,22 +37,36 @@ func (s *StepImageBuilderSnapshot) Run(ctx context.Context, state multistep.Stat
 
 	ui.Say("Launching an AppStream ImageBuilder...")
 
-	_ = s.config.Tags
-	cmd := &packersdk.RemoteCmd{}
-	comm.Start(ctx, cmd)
-	cmd.Wait()
-	// TODO: Somehow have powershell implement something similar to the following:
-	// provisioner "windows-shell" {
-	//   inline = [
-	//     "start \"\" /B \"image-assistant.exe\" \"create-image\" \"--name=${s.name}\" \"--tags=${join(" ", [for k, v in s.config.Tags : "\"${k}\" \"${v}\""])}\""
-	//   ]
-	// }
+	// Construct the command to create the image
+	cmdString := fmt.Sprintf("image-assistant.exe create-image --name %s", s.config.Name)
+	if len(s.config.Tags) > 0 {
+		cmdString += " --tags"
+		for k, v := range s.config.Tags {
+			cmdString += fmt.Sprintf(" %q %q", k, v)
+		}
+	}
+
+	ui.Say(fmt.Sprintf("Executing command: %s", cmdString))
+
+	cmd := &packersdk.RemoteCmd{
+		Command: cmdString,
+	}
+
+	if err := cmd.RunWithUi(ctx, comm, ui); err != nil {
+		state.Put("error", fmt.Errorf("failed to run image creation command: %w", err))
+		return multistep.ActionHalt
+	}
+
+	if cmd.ExitStatus() != 0 {
+		state.Put("error", fmt.Errorf("image creation command failed with exit status: %d", cmd.ExitStatus()))
+		return multistep.ActionHalt
+	}
 
 	// Wait for image to become available
 	var elapsed time.Duration
 	for {
 		images, err := svc.DescribeImages(ctx, &appstream.DescribeImagesInput{
-			Names: []string{"TODO-IMPLEMENT-NAME"},
+			Names: []string{s.config.Name},
 			Type:  types.VisibilityTypePrivate,
 		})
 
@@ -60,15 +75,34 @@ func (s *StepImageBuilderSnapshot) Run(ctx context.Context, state multistep.Stat
 			return multistep.ActionHalt
 		}
 
+		if len(images.Images) == 0 {
+			// Image might not be immediately available after command returns
+			ui.Say(fmt.Sprintf("Waiting for image %s to appear... (elapsed: %s)", s.config.Name, elapsed))
+			wait := 10 * time.Second
+			elapsed += wait
+			time.Sleep(wait)
+			continue
+		}
+
 		switch images.Images[0].State {
 		case types.ImageStateAvailable:
+			state.Put("images", map[string]string{
+				s.config.RawRegion: s.config.Name,
+			})
 			return multistep.ActionContinue
 		case types.ImageStateFailed:
 			state.Put("error", fmt.Errorf("ImageBuilder failed"))
 			return multistep.ActionHalt
 		case types.ImageStatePending:
 			ui.Say(fmt.Sprintf("Waiting for ImageBuilder to become available (elapsed: %s)", elapsed))
-			wait := 5 * time.Second
+			wait := 10 * time.Second
+			elapsed += wait
+			time.Sleep(wait)
+			continue
+		default:
+			// Handle other states if necessary, or just wait
+			ui.Say(fmt.Sprintf("Image state is %s, waiting... (elapsed: %s)", images.Images[0].State, elapsed))
+			wait := 10 * time.Second
 			elapsed += wait
 			time.Sleep(wait)
 			continue
